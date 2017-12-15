c74e9771c961293cabe38e19b8bd23cb0fbd9438
sridharchandrasekaran <sridhar.onwardpath@gmail.com>
2017-12-15 15:57:00 +0530

    commit computer package to repository

---
+++ computer/src/computer/Process.java
@@ -0,0 +1,18 @@
+package computer;
+
+import onwadpath.Path;
+
+
+
+public class Process {
+
+	public static void main(String[] args) {
+		Path p=new Path();
+		p.show();
+
+	}
+	public void test(){
+		System.out.println("testing");
+	}
+
+}
